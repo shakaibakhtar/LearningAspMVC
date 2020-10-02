223da9ddd9ff32ac4ca838f250ac7d7bedc0569d
shakaib.developer <shakaib.developer@gmail.com>
2020-10-02 21:49:38 +0500

    Add project files.

---
+++ Learning/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learning.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LoginWithGoogle()
+        {
+            return View();
+        }
+    }
+}
