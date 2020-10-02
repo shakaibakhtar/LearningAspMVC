bf9e44a9a40ec106a2011faee478caa34e4f3eaf
shakaib.developer <shakaib.developer@gmail.com>
2020-10-03 00:17:19 +0500

    Signup & Login Using Pure MVC

--- Learning/Controllers/AccountController.cs
+++ Learning/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Learning.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,8 @@ namespace Learning.Controllers
 {
     public class AccountController : Controller
     {
+        LearningDbEntities db = new LearningDbEntities();
+
         // GET: Account
         public ActionResult Register()
         {
@@ -15,13 +19,54 @@ namespace Learning.Controllers
         }
 
         [HttpPost]
-        public ActionResult Register()
+        public ActionResult Register(UserModel user)
+        {
+            try
+            {
+                User dbUser = new User() { Full_Name = user.Full_Name, Email = user.Email, Password = user.Password };
+
+                if(db.Users.Where(x => x.Email.Equals(user.Email)).Count() > 0)
+                {
+                    ViewBag.msg = "User already exist";
+                }
+                else
+                {
+                    db.Users.Add(dbUser);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Login");
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = ex.StackTrace;
+            }
+            return View();
+        }
+
+        public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult LoginWithGoogle()
+        [HttpPost]
+        public ActionResult Login(UserModel user)
         {
+            try
+            {
+                if(db.Users.Where(x=>x.Email.Equals(user.Email) && x.Password.Equals(user.Password)).Count() > 0)
+                {
+                    ViewBag.msg = "Login Successfull";
+                }
+                else
+                {
+                    ViewBag.msg = "Invalid username or password";
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = ex.StackTrace;
+            }
             return View();
         }
     }
--- Learning/Models/UserModel.cs
+++ Learning/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,18 @@ namespace Learning.Models
 {
     public class UserModel
     {
-        public int MyProperty { get; set; }
+        public int Id { get; set; }
+        
+        [Required]
+        public string Full_Name { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        public string ConfirmPassword { get; set; }
     }
 }
